f72cd5fe7ab8f2d072e28db49627a519a1521032
joao <joao>
2020-01-14 15:11:02 -0300

    first commit

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn poraoBar.wsgi --log-file -
---
+++ poraoBar/core/tests.py
@@ -0,0 +1,12 @@
+from django.test import TestCase
+from django.shortcuts import resolve_url as r
+
+class Hometest(TestCase):
+    def setUp(self):
+        self.response = self.client.get(r('home'))
+
+    def test_get(self):
+        self.assertEqual(200, self.response.status_code)
+
+    def test_template(self):
+        self.assertTemplateUsed(self.response, 'index.html')
