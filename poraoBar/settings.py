fd9acf9816fd159850c554b35aadb892d83d2956
joaoVittorDevv <joaovittorh@hotmail.com>
2020-09-29 17:50:27 -0300

    .

--- poraoBar/settings.py
+++ poraoBar/settings.py
@@ -32,6 +32,8 @@ ALLOWED_HOSTS = config('ALLOWED_HOSTS', default=[], cast=Csv())
 # Application definition
 
 INSTALLED_APPS = [
+    'admin_interface',
+    'colorfield',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -42,6 +44,8 @@ INSTALLED_APPS = [
     'analytical',
     'poraoBar.core',
 ]
+
+X_FRAME_OPTIONS='SAMEORIGIN'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
